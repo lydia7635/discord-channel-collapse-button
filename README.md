fb8a989fb66fa24869a86d8c282c10e19abe8f6a
lydia7635 <lydia7635@gmail.com>
2022-01-25 18:51:00 +0800

    Fix bug (not working) in main.js

--- README.md
+++ README.md
@@ -5,10 +5,10 @@ A small snippit that creates a button to hide the channel list on Discord
 ## How To Use
 ### For BetterDiscord Client Users
 1. Download [channelcollapsebutton.plugin.js](https://github.com/IRDeNial/discord-hide-channel-list/blob/master/channelcollapsebutton.plugin.js)
-2. Put it in your plugins directory (Just open it from the client, can vary between indivudla users).
+2. Put it in your plugins directory (Just open it from the client, can vary between individual users).
 3. Restart BetterDiscord
 
-### For Normal Discord Client Users:
+### For Normal Discord Client Users (with developer console):
 1. Go into your discord client settings and enable developer mode.
   * This can be accessed under the Appearance area.
 2. Open the developer console.
@@ -17,6 +17,12 @@ A small snippit that creates a button to hide the channel list on Discord
 3. Paste in the code from [main.js](https://github.com/IRDeNial/discord-hide-channel-list/blob/master/main.js) into the **Console** tab
 4. Press enter
 5. Enjoy the newly added feature
+
+### For Normal Discord Client Users (with bookmark):
+1. Add a new bookmark in your browser
+  * This can vary depending on your browser.  It would be the same way you open it in Chrome or FireFox.
+2. Paste in the code from [main.js](https://github.com/IRDeNial/discord-hide-channel-list/blob/master/main.js) into URL content, and add `javascript:` in the beginning.
+3. Select the bookmark and enjoy it.
 
 ## Contribution Guidelines
 * Document **ALL** code contributions
--- main.js
+++ main.js
@@ -2,35 +2,35 @@
     var ChannelCollapse = {
         addToolbarItem: function() {
             if(document.querySelectorAll('.show-channels-button').length == 0) {
-                var newToolbarItem = document.createElement('span');
+                let newToolbarItem = document.createElement('div');
+                newToolbarItem.classList.add(document.querySelector('[class*="toolbar"] > [class*="iconWrapper"]').classList[1]);
+                newToolbarItem.classList.add(document.querySelector('[class*="toolbar"] > [class*="clickable"]').classList[2]);
+                
                 newToolbarItem.classList.add('show-channels-button');
                 newToolbarItem.classList.add('active');
-                newToolbarItem.classList.add(document.querySelector('[class*="toolbar"]').classList[0])
-                newToolbarItem.style.width = '26px'
-                newToolbarItem.style.position = 'relative'
-                newToolbarItem.style.top = '-2px'
-                newToolbarItem.style.display = 'flex'
-                newToolbarItem.style.flexDirection = 'column'
-                newToolbarItem.style.justifyContent = 'flex-start'
-                newToolbarItem.style.cursor = 'pointer'
-                newToolbarItem.innerHTML = '<span class="show-channels-button toolbar-1t6TWx active" style="width: 26px; position: relative; top: -2px; display: flex; flex-direction: column; justify-content: flex-start; cursor: pointer;"><span class="show-channels-button toolbar-1t6TWx" style="width: 26px;position: relative;top: -6px;display: flex;flex-direction: column;justify-content: flex-start;cursor: pointer;font-family: serif !important;"><span class="show-channels-button toolbar-1t6TWx active" style="position: relative;top: -2px;display: flex;flex-direction: column;justify-content: flex-start;cursor: pointer;padding-right: 30px;padding-bottom: 25px;"><span class="show-channels-button toolbar-1t6TWx active" style="position: initial;display: flex;flex-direction: column;justify-content: flex-start;cursor: pointer;"><span class="show-channels-button toolbar-1t6TWx active" style="position: absolute;display: flex;flex-direction: column;justify-content: flex-start;cursor: pointer;margin-bottom: 15px;"><span style="display: block;color: var(--text-normal);-webkit-transform: scale(2.0, 1.0);-moz-transform:scale(2.0, 1.0);-ms-transform:scale(2.0, 1.0);-o-transform:scale(2.0, 1.0);transform: scale(5,2);width: 0;height: 1px;text-align: center;">-</span><span style="display: block; color: var(--text-normal); -webkit-transform: scale(5.0, 1.0);-moz-transform:scale(2.0, 1.0);-ms-transform:scale(2.0, 1.0);-o-transform:scale(2.0, 1.0);transform: scale(5,2);width: 0;height: 19px;">-</span><span style="display: block; color: var(--text-normal); -webkit-transform: scale(2.0, 1.0);-moz-transform:scale(2.0, 1.0);-ms-transform:scale(2.0, 1.0);-o-transform:scale(2.0, 1.0);transform: scale(5,2);width: 0;padding-bottom: 5px;">-</span>';
 
-                document.querySelector('[class*="toolbar"]').insertBefore(newToolbarItem,document.querySelector('.search-36MZv-'));
-
-                document.querySelector('.show-channels-button').addEventListener('click',function(e){
+                newToolbarItem.setAttribute("role", "button");
+                newToolbarItem.setAttribute("aria-label", "Collapse Sidebar");
+                newToolbarItem.setAttribute("tabindex", "0");
+                
+                newToolbarItem.innerHTML = '<svg x="0" y="0" class="icon-2xnN2Y" aria-hidden="false" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M1 4 H 21 V 7 H 1 Z"></path><path fill="currentColor" d="M1 11 H 21 V 14 H 1 Z"></path><path fill="currentColor" d="M1 18 H 21 V 21 H 1 Z"></path></svg>';
+                
+                document.querySelector('[class*="toolbar"]').insertBefore(newToolbarItem, document.querySelector('[class*="search"]'));
+                document.querySelector('.show-channels-button').addEventListener('click', function(){
                     if(this.classList.contains('active')) {
-                        document.querySelector('.sidebar-2K8pFh').style.display = 'none';
+                        document.querySelector('[class*="sidebar"]').style.display = 'none';
                         this.classList.toggle('active');
                     } else {
-                        document.querySelector('.sidebar-2K8pFh').style.display = 'flex';
+                        document.querySelector('[class*="sidebar"]').style.display = 'flex';
                         this.classList.toggle('active');
                     }
                 });
             }
         },
+
         addEventListeners: function(){
             var guilds = document.querySelectorAll('[class^="guild"]');
-            for(var i = 0;i < guilds.length; ++i) {
+            for(let i = 0; i < guilds.length; ++i) {
                 guilds[i].addEventListener('click',function(){
                     window.setTimeout(function(){
                         console.log("Added event listener");
